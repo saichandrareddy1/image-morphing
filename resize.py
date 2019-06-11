9a94332b3af8000ec68dae90d841229fc27bb380
sai chandra reddy <38432324+saichandrareddy1@users.noreply.github.com>
2019-06-11 22:27:06 +0530

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,7 +1,11 @@
-import os
 from PIL import Image
+from imutils import paths
+import requests
+import cv2
+import os
 
-path = os.path.abspath("./images")
+
+path = os.path.abspath(r"C:\Users\Sai Chandra Reddy\Desktop\Sports Vision\YellowCards")
 def get_imlist(path):
   """Returns a list of filenames for
     all jpg images in a directory. """
@@ -15,9 +19,25 @@ for i in c:
     if i.endswith('.jpg'):
         try:
             im = Image.open(i)
-            print("HII")
             im = im.resize((600, 600),Image.ANTIALIAS)
-            im.save(r"C:\Users\Sai Chandra Reddy\Desktop\Crickters\new images\0000"+str(s)+".jpg")
+            im.save(r"C:\Users\Sai Chandra Reddy\Desktop\Sports Vision Resized\NewYellowcards\0000"+str(s)+".jpg")
         except IOError:
-            print("Bye")
             print ("cannot create thumbnail for '%s'" % i)
+
+for imagePath in paths.list_images(r"C:\Users\Sai Chandra Reddy\Desktop\Sports Vision Resized\NewYellowcards"):
+  
+  delete = False
+  
+  try:
+    image = cv2.imread(imagePath)
+    
+    if image is None:
+      delete = True
+      
+  except:
+    print("Except")
+    delete = True
+        
+  if delete:
+    print("[INFO] deleting {}".format(imagePath))
+    os.remove(imagePath)
